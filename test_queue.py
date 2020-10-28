45f83c3a253506128e5cc8c0004e6db094c24eb3
DragonController <dan.h.fowler@gmail.com>
2020-10-27 21:34:42 -0700

    test_enqueue_efficiency working

--- test_queue.py
+++ test_queue.py
@@ -184,30 +184,30 @@ class TestQueue(unittest.TestCase):
     Algorithmic complexity
     """
 
-    # def test_enqueue_efficiency(self):
-    #     """
-    #     Enqueing a value is always O(1).
-    #     """
-    #     time_samples = []
-    #     for _ in range(0, 1000):
-    #         q = Queue()
-    #         start_time = time.time()
-    #         q.enqueue('fake')
-    #         end_time = time.time()
-    #         time_samples.append(end_time - start_time)
-    #     small_average_enqueue_time = sum(time_samples) / float(len(time_samples))
+    def test_enqueue_efficiency(self):
+        """
+        Enqueing a value is always O(1).
+        """
+        time_samples = []
+        for _ in range(0, 1000):
+            q = Queue()
+            start_time = time.time()
+            q.enqueue('fake')
+            end_time = time.time()
+            time_samples.append(end_time - start_time)
+        small_average_enqueue_time = sum(time_samples) / float(len(time_samples))
 
-    #     large_queue = Queue()
-    #     for _ in range(0, 1000000):
-    #         large_queue.enqueue('fake')
-    #     large_time_samples = []
-    #     for _ in range(0, 1000):
-    #         start_time = time.time()
-    #         large_queue.enqueue('fake')
-    #         end_time = time.time()
-    #         large_time_samples.append(end_time - start_time)
-    #     large_average_enqueue_time = sum(large_time_samples) / float(len(large_time_samples))
-    #     self.assertAlmostEqual(small_average_enqueue_time, large_average_enqueue_time, delta=small_average_enqueue_time)
+        large_queue = Queue()
+        for _ in range(0, 1000000):
+            large_queue.enqueue('fake')
+        large_time_samples = []
+        for _ in range(0, 1000):
+            start_time = time.time()
+            large_queue.enqueue('fake')
+            end_time = time.time()
+            large_time_samples.append(end_time - start_time)
+        large_average_enqueue_time = sum(large_time_samples) / float(len(large_time_samples))
+        self.assertAlmostEqual(small_average_enqueue_time, large_average_enqueue_time, delta=small_average_enqueue_time)
 
     # def test_dequeue_efficiency(self):
     #     """
